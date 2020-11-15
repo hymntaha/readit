99ecebff858cf416ba71098ee086a2a07031f483
hymntaha <tahauygun@gmail.com>
2020-11-15 15:20:17 -0800

    fixes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-
+  await orm.getMigrator().up();
   const post = orm.em.create(Post, {
     title: "First post",
   });
