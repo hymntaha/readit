c5e8b99f8b3d7cf02094dd8fc0602e7c9ba312d7
hymntaha <tahauygun@gmail.com>
2020-11-15 15:27:06 -0800

    some work

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, {
-    title: "First post",
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [],
+      validate: false,
+    }),
   });
 
-  await orm.em.persistAndFlush(post);
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+
+  app.listen(4000, () => {
+    console.log("Listening on 4000");
+  });
 };
 
 main().catch((err) => console.error(err));
